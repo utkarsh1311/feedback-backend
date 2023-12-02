38e27769cb3fca9e7c64c41d22231f780d3d1eba
utkarsh maurya <itsutkarsh123@gmail.com>
2023-12-02 20:40:55 +0530

    add routes to app

--- app.ts
+++ app.ts
@@ -1,6 +1,10 @@
 import cors from "cors";
 import express from "express";
 import morgan from "morgan";
+import errorHandler from "./middleware/errorHandler";
+import feedbackRouter from "./routes/feedbackRouter";
+import "express-async-errors";
+import teacherRouter from "./routes/teacherRouter";
 
 const app = express();
 
@@ -9,9 +13,11 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {});
 
+app.use("/feedbacks", feedbackRouter);
+app.use("/teachers", teacherRouter);
 
-app.get("/", (req, res) => {
-  
-});
+app.use(errorHandler);
+
 export default app;
--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+
+
 import app from "./app";
 
 app.listen(3000, () => {
